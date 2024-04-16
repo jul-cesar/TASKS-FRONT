67756fd82728873e7473142bd8c9a10784ebf783
Julio Cesar <juliocesarmartinez1344@gmail.com>
2024-04-15 20:54:25 -0500

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,44 @@ import PersistLogin from "./utils/PersistLogin";
 import { privateRoutes, publicRoutes } from "./models/routes";
 import { lazy, Suspense } from "react";
 import { UiContextProvider } from "./context/ui";
+import Profile from "./pages/Profile";
+import { AuthProvider } from "./context/auth";
+import { ThemeProvider } from "./components/theme-provider";
+import { Toaster } from "sonner";
+import Login from "./pages/LogIn";
+import Register from "./pages/Register";
 
 function App() {
-  const LogInLazy = lazy(() => import("./pages/LogIn"));
-  const RegisterLazy = lazy(() => import("./pages/Register"));
   const TasksPageLazy = lazy(() => import("./pages/TasksPage"));
   const AsignedPageLazy = lazy(() => import("./pages/AsignedPage"));
-  const ProfileLazy = lazy(() => import("./pages/Profile"));
 
   return (
     <UiContextProvider>
-      <Suspense>
-        <Routes>
-          <Route element={<PersistLogin />}>
-            <Route element={<ProtectedRoute />}>
-              <Route path={privateRoutes.TASKS} element={<TasksPageLazy />} />
-              <Route
-                path={privateRoutes.ASIGNEDTASKS}
-                element={<AsignedPageLazy />}
-              />
-            </Route>
-          </Route>
-          <Route path={publicRoutes.LOGIN} element={<LogInLazy />} />
-          <Route path={publicRoutes.REGISTER} element={<RegisterLazy />} />
-          <Route path={publicRoutes.PROFILE} element={<ProfileLazy />} />
-          <Route path="*" element={<>Not Found</>} />
-        </Routes>
-      </Suspense>
+      <AuthProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Toaster richColors />
+          <Suspense>
+            <Routes>
+              <Route element={<PersistLogin />}>
+                <Route element={<ProtectedRoute />}>
+                  <Route
+                    path={privateRoutes.TASKS}
+                    element={<TasksPageLazy />}
+                  />
+                  <Route
+                    path={privateRoutes.ASIGNEDTASKS}
+                    element={<AsignedPageLazy />}
+                  />
+                </Route>
+              </Route>
+              <Route path={publicRoutes.LOGIN} element={<Login />} />
+              <Route path={publicRoutes.REGISTER} element={<Register />} />
+              <Route path={publicRoutes.PROFILE} element={<Profile />} />
+              <Route path="*" element={<>Not Found</>} />
+            </Routes>
+          </Suspense>
+        </ThemeProvider>
+      </AuthProvider>
     </UiContextProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/auth.tsx";
-import { Toaster } from "sonner";
-import { ThemeProvider } from "./components/theme-provider.tsx";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Toaster richColors />
+  
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <React.StrictMode>
@@ -20,6 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </React.StrictMode>
         </BrowserRouter>
       </QueryClientProvider>
-    </ThemeProvider>
-  </AuthProvider>
+  
 );
