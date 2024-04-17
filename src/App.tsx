d9efff72dc616d3022051394c764677d5137bc84
Julio Cesar <juliocesarmartinez1344@gmail.com>
2024-04-16 19:58:21 -0500

    suspense

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,48 @@ import { ThemeProvider } from "./components/theme-provider";
 import { Toaster } from "sonner";
 import Login from "./pages/LogIn";
 import Register from "./pages/Register";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+import LoaderMedium from "./components/loaders/LoaderMedium";
 
 function App() {
   const LazyTaskPage = lazy(() => import("../src/pages/TasksPage"));
   const LazyAsignsPage = lazy(() => import("../src/pages/AsignedPage"));
 
   return (
-    <UiContextProvider>
-      <AuthProvider>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Toaster richColors />
+    <Suspense
+      fallback={
+        <div className="h-screen flex justify-center items-center">
+          <LoaderMedium />
+        </div>
+      }
+    >
+      <UiContextProvider>
+        <AuthProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Toaster richColors />
 
-          <Routes>
-            <Route element={<PersistLogin />}>
-              <Route element={<ProtectedRoute />}>
-                <Route path={privateRoutes.TASKS} element={<LazyTaskPage />} />
-                <Route
-                  path={privateRoutes.ASIGNEDTASKS}
-                  element={<LazyAsignsPage />}
-                />
+            <Routes>
+              <Route element={<PersistLogin />}>
+                <Route element={<ProtectedRoute />}>
+                  <Route
+                    path={privateRoutes.TASKS}
+                    element={<LazyTaskPage />}
+                  />
+                  <Route
+                    path={privateRoutes.ASIGNEDTASKS}
+                    element={<LazyAsignsPage />}
+                  />
+                </Route>
               </Route>
-            </Route>
-            <Route path={publicRoutes.LOGIN} element={<Login />} />
-            <Route path={publicRoutes.REGISTER} element={<Register />} />
-            <Route path={publicRoutes.PROFILE} element={<Profile />} />
-            <Route path="*" element={<>Not Found</>} />
-          </Routes>
-        </ThemeProvider>
-      </AuthProvider>
-    </UiContextProvider>
+              <Route path={publicRoutes.LOGIN} element={<Login />} />
+              <Route path={publicRoutes.REGISTER} element={<Register />} />
+              <Route path={publicRoutes.PROFILE} element={<Profile />} />
+              <Route path="*" element={<>Not Found</>} />
+            </Routes>
+          </ThemeProvider>
+        </AuthProvider>
+      </UiContextProvider>
+    </Suspense>
   );
 }
 
