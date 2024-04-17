5204e3c984fbfb382dc4fdb181237bad4b9b656d
Julio Cesar <juliocesarmartinez1344@gmail.com>
2024-04-17 01:34:11 -0500

    23esd

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,15 @@ import "./App.css";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PersistLogin from "./utils/PersistLogin";
 import { privateRoutes, publicRoutes } from "./models/routes";
-import { UiContextProvider } from "./context/ui";
-import { AuthProvider } from "./context/auth";
-import { ThemeProvider } from "./components/theme-provider";
-import { Toaster } from "sonner";
-import Login from "./pages/LogIn";
-import Register from "./pages/Register";
+
 import { lazy, Suspense } from "react";
 import LoaderMedium from "./components/loaders/LoaderMedium";
 import TasksPage from "./pages/TasksPage";
 
 function App() {
   const LazyAsignsPage = lazy(() => import("../src/pages/AsignedPage"));
+  const LazyLoginPage = lazy(() => import("../src/pages/LogIn"));
+  const LazyRegisterPage = lazy(() => import("../src/pages/Register"));
   const LazyProfilePage = lazy(() => import("../src/pages/Profile"));
 
   return (
@@ -25,35 +22,22 @@ function App() {
         </div>
       }
     >
-      <UiContextProvider>
-        <AuthProvider>
-          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <Toaster richColors />
+      <Routes>
+        <Route element={<PersistLogin />}>
+          <Route element={<ProtectedRoute />}>
+            <Route path={privateRoutes.TASKS} element={<TasksPage />} />
+            <Route
+              path={privateRoutes.ASIGNEDTASKS}
+              element={<LazyAsignsPage />}
+            />
+          </Route>
+        </Route>
 
-            <Routes>
-              <Route element={<PersistLogin />}>
-                <Route element={<ProtectedRoute />}>
-                  <Route
-                    path={privateRoutes.TASKS}
-                    element={<TasksPage />}
-                  />
-                  <Route
-                    path={privateRoutes.ASIGNEDTASKS}
-                    element={<LazyAsignsPage />}
-                  />
-                </Route>
-              </Route>
-              <Route path={publicRoutes.LOGIN} element={<Login />} />
-              <Route path={publicRoutes.REGISTER} element={<Register />} />
-              <Route
-                path={publicRoutes.PROFILE}
-                element={<LazyProfilePage />}
-              />
-              <Route path="*" element={<>Not Found</>} />
-            </Routes>
-          </ThemeProvider>
-        </AuthProvider>
-      </UiContextProvider>
+        <Route path={publicRoutes.LOGIN} element={<LazyLoginPage />} />
+        <Route path={publicRoutes.REGISTER} element={<LazyRegisterPage />} />
+        <Route path={publicRoutes.PROFILE} element={<LazyProfilePage />} />
+        <Route path="*" element={<>Not Found</>} />
+      </Routes>
     </Suspense>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,26 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { UiContextProvider } from "./context/ui.tsx";
+import { AuthProvider } from "./context/auth.tsx";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        </BrowserRouter>
-      </QueryClientProvider>
-  
+  <AuthProvider>
+    <UiContextProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <QueryClientProvider client={queryClient}>
+          <Toaster richColors />
+          <BrowserRouter>
+            <React.StrictMode>
+              <App />
+            </React.StrictMode>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </UiContextProvider>
+  </AuthProvider>
 );
