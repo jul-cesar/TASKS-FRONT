157d59cb898a644b189106cbff2404d396922c39
Julio Cesar <juliocesarmartinez1344@gmail.com>
2024-04-18 00:52:29 -0500

    lazyless test

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,27 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import PersistLogin from "./utils/PersistLogin";
 import { privateRoutes, publicRoutes } from "./models/routes";
 
-import { lazy, Suspense } from "react";
-import LoaderMedium from "./components/loaders/LoaderMedium";
+import TasksPage from "./pages/TasksPage";
+import AsignedPage from "./pages/AsignedPage";
+import Login from "./pages/LogIn";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
-  const AsignedPage = lazy(() => import("../src/pages/AsignedPage"));
-  const Login = lazy(() => import("../src/pages/LogIn"));
-  const Register = lazy(() => import("../src/pages/Register"));
-  const Profile = lazy(() => import("../src/pages/Profile"));
-  const TasksPage = lazy(() => import("./pages/TasksPage"));
-
   return (
-    <Suspense
-      fallback={
-        <div className="h-screen flex justify-center items-center">
-          <LoaderMedium />
-        </div>
-      }
-    >
-      <Routes>
-        <Route element={<PersistLogin />}>
-          <Route element={<ProtectedRoute />}>
-            <Route path={privateRoutes.TASKS} element={<TasksPage />} />
-            <Route
-              path={privateRoutes.ASIGNEDTASKS}
-              element={<AsignedPage />}
-            />
-          </Route>
+    <Routes>
+      <Route element={<PersistLogin />}>
+        <Route element={<ProtectedRoute />}>
+          <Route path={privateRoutes.TASKS} element={<TasksPage />} />
+          <Route path={privateRoutes.ASIGNEDTASKS} element={<AsignedPage />} />
         </Route>
+      </Route>
 
-        <Route path={publicRoutes.LOGIN} element={<Login />} />
-        <Route path={publicRoutes.REGISTER} element={<Register />} />
-        <Route path={publicRoutes.PROFILE} element={<Profile />} />
-        <Route path="*" element={<>Not Found</>} />
-      </Routes>
-    </Suspense>
+      <Route path={publicRoutes.LOGIN} element={<Login />} />
+      <Route path={publicRoutes.REGISTER} element={<Register />} />
+      <Route path={publicRoutes.PROFILE} element={<Profile />} />
+      <Route path="*" element={<>Not Found</>} />
+    </Routes>
   );
 }
 
